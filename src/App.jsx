0c8a76fb67da241972b338cb17575bdcd6bf59b2
ahmed <a.salawu@outlook.com>
2024-02-20 09:51:21 +0000

    added search bar, menu & logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-  <h1 className='text-3xl text-red-900 underline font-bold'>Welcome to my restaurant project</h1>  
+    <Navbar />
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
+
+const Navbar = () => {
+  return (
+    <div className="max-w-[1640px] flex justify-between items-center mx-auto p-4">
+      <div className="flex items-center">
+        <div>
+          <AiOutlineMenu size={20} className="cursor-pointer" />
+        </div>
+        <h1 className="text-xl sm:text-3xl lg:text-4xl px-2">
+          Best <span className="font-bold">Eats</span>
+        </h1>
+        <div className="hidden lg:flex items-center bg-gray-200 text-[14px] rounded-full">
+          <p className="bg-black rounded-full p-2 text-white">Delivery</p>
+          <p className="p-2">Pick up</p>
+        </div>
+      </div>
+
+      {/* search bar */}
+      <div className="bg-gray-200 px-2 rounded-full flex items-center w-[200px] sm:w-[400px] lg:w-[500px]">
+        <AiOutlineSearch size={25}/>
+        <input type="text" placeholder="Search Foods" className="bg-transparent p-2 focus:outline-none w-full"/>
+      </div>
+    </div>
+
+  );
+};
+
+export default Navbar;
