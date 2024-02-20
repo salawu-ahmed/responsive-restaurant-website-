d174e692e8ba858fde0a1a2c795fd5ce616a4390
ahmed <a.salawu@outlook.com>
2024-02-20 11:14:25 +0000

    added HeadlineCards

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import HeadlineCards from './components/HeadlineCards'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <>
     <Navbar />
     <Hero />
+    <HeadlineCards />
     </>
   )
 }
---
+++ src/components/HeadlineCards.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const HeadlineCards = () => {
+  return (
+    <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-4'>
+      {/* card */}
+      <div className='rounded-xl relative'>
+        {/* overlay */}
+        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+            <p className='font-bold text-2xl px-2 pt-5'>Sun's Out, BOGO's Out</p>
+            <p className='px-2'>Through 8/26</p>
+            <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+        </div>
+        <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src="https://images.pexels.com/photos/566566/pexels-photo-566566.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
+      </div>
+
+      {/* card */}
+      <div className='rounded-xl relative'>
+        {/* overlay */}
+        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+            <p className='font-bold text-2xl px-2 pt-5'>We Deliver Deserts</p>
+            <p className='px-2'>Tasty Treats</p>
+            <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+        </div>
+        <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src="https://images.pexels.com/photos/566566/pexels-photo-566566.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
+      </div>
+
+      {/* card */}
+      <div className='rounded-xl relative'>
+        {/* overlay */}
+        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+            <p className='font-bold text-2xl px-2 pt-5'>New Restaurants</p>
+            <p className='px-2'>Added Daily</p>
+            <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+        </div>
+        <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src="https://images.pexels.com/photos/566566/pexels-photo-566566.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
+      </div>
+    </div>
+  )
+}
+
+export default HeadlineCards
